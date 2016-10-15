336c93ea173e56aab50cf1bf4503e9b35d41ad92
Jonathan Pineda <jonathan.pineda@mayor.cl>
2016-10-14 21:43:40 -0300

    Primer commit de Jpineda

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         echo "Hola Mundo";
+        echo "Este es un cambio de Jpineda para su primer commit";
         ?>
     </body>
 </html>
